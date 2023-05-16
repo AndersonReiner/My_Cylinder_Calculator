2901f3c818dd20c27393d9182bc4da4166ef41b5
AndersonReiner <108676604+AndersonReiner@users.noreply.github.com>
2023-05-15 22:59:09 -0400

    second commit
    segundo commit

--- MyCylindercalculator/app/src/main/java/com/example/mycylindercalculator/Area2.java
+++ MyCylindercalculator/app/src/main/java/com/example/mycylindercalculator/Area2.java
@@ -1,14 +1,21 @@
 package com.example.mycylindercalculator;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.SparseIntArray;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Area2 extends AppCompatActivity {
-    Button btn_voltar;
+    Button btn_voltar, btn_calcular;
+    EditText edt_raio,edt_alt;
+    TextView edt_res;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,27 @@ public class Area2 extends AppCompatActivity {
         setContentView(R.layout.activity_area2);
 
         btn_voltar = findViewById(R.id.btn_voll);
+        btn_calcular = findViewById(R.id.btn_calcular);
+        edt_raio = findViewById(R.id.edt_raio);
+        edt_alt = findViewById(R.id.edt_alt);
+        edt_res = findViewById(R.id.edt_res);
+
+        edt_raio.requestFocus();
+
+        btn_calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences preferences = getSharedPreferences("chaveGeral", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("chaveRaio", edt_raio.getText().toString());
+                editor.putString("chaveAltura", edt_raio.getText().toString());
+                editor.commit();
+                Toast.makeText(Area2.this,"Seus daddos foram roubados!!!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
 
         btn_voltar.setOnClickListener(new View.OnClickListener() {
             @Override
--- MyCylindercalculator/app/src/main/java/com/example/mycylindercalculator/Historico.java
+++ MyCylindercalculator/app/src/main/java/com/example/mycylindercalculator/Historico.java
@@ -2,20 +2,31 @@ package com.example.mycylindercalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.badge.BadgeUtils;
 
 public class Historico extends AppCompatActivity {
 
     Button btn_voll;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historico);
+
+        result = findViewById(R.id.result);
+
+        String raio, altura;
+        SharedPreferences preferences = getSharedPreferences("chaveGeral", MODE_PRIVATE);
+        raio = preferences.getString("chavaRaio","");
+        altura = preferences.getString("chavealtura","");
+        result.setText("hello teste" + raio + altura);
 
         btn_voll = findViewById(R.id.btn_voll);
 
